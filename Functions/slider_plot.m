c3dfeee2af579a941268520a8f0298762e623b31
Corentin Wicht <corentin.wicht@unifr.ch>
2020-10-09 15:27:37 +0200

    Major path
    - removed the possibility to specify specific electrodes (useless)
    - Improved the sliderbar, now also updates the two other graphs
    - Enabled the possibility to specify the extension of the files to be loaded

--- Functions/slider_plot.m
+++ Functions/slider_plot.m
@@ -1,4 +1,4 @@
-function slider_plot(f,EEGTEMP,MaxPos,Chanlocs)
+function slider_plot(f,EEGTEMP,MaxPos,Chanlocs,Param,TEMPGFP)
 % Function for generation of topoplots using a slidebar
 
 % Corentin Wicht
@@ -16,7 +16,8 @@ bgcolor = f.Color; % background color
 % SLIDER
 PosSlide = [0.64 0.1 0.30 0.03];
 S.aSlider  = uicontrol('Parent',f,'Style','slider','unit','normalized','Position',PosSlide,...
-              'value',MaxPos, 'min',0, 'max',size(EEGTEMP,1),'callback', {@SliderCB,EEGTEMP,Chanlocs});
+              'value',MaxPos, 'min',0, 'max',size(EEGTEMP,1),'SliderStep',[1/size(EEGTEMP,1) 10/size(EEGTEMP,1)],...
+              'callback', {@SliderCB,EEGTEMP,Chanlocs,Param,TEMPGFP});
           
 % LABELS
 bl1 = uicontrol('Parent',f,'Style','text','unit','normalized','Position',[0.63 0.095 0.01 0.03],...
@@ -28,9 +29,35 @@ bl3 = uicontrol('Parent',f,'Style','text','unit','normalized','Position',[0.75 0
 end
 
 % Callback for all sliders defined above
-function SliderCB(aSlider, EventData,EEGTEMP,Chanlocs)
+function SliderCB(aSlider, EventData,EEGTEMP,Chanlocs,Param,TEMPGFP)
     Value = round(get(aSlider, 'Value'));  % Get the slidebar value
     assignin('base','SlideBarValue',Value);
+    
+    % 1. Electrodes level data
+    subplot(2,3,[1 2]); 
+    plot(EEGTEMP,'Color','k'); 
+    title('All electrodes');
+    xlabel('Time (ms)'), ylabel('uV'); axis tight; Yl=ylim(gca);  
+    set(gca,'XTick',Param.Ticks,'XTickLabel',num2cell(Param.XTStr)); 
+    PosinMS = round(Value/Param.SamplingRate*1000);
+    LabelValue = [num2str(PosinMS + Param.Epoch(1)) 'ms ' '[' num2str(Value) 'TF' ']']; 
+    xline(Value,'-',LabelValue,'Color','b','LabelVerticalAlignment','bottom');
+    PosRectinTF = [Param.TEMPCompinTF(1) Yl(1) Param.TEMPCompinTF(2)-Param.TEMPCompinTF(1) Yl(2)*0.1]; 
+    rectangle('Position',PosRectinTF,'FaceColor',[0 0 1 0.2],'EdgeColor',[0 0 1 0.2]);
+    
+    % 2. GFP
+    subplot(2,3,[4 5]); plot(TEMPGFP, 'Color','k'); 
+    title('GFP'); % axis tight;
+    axis tight; Yl=ylim(gca); % retrieve auto y-limits
+    xlabel('Time (ms)'), ylabel('GFP');
+    set(gca,'XTick',Param.Ticks,'XTickLabel',num2cell(Param.XTStr));
+    xline(Value,'-',LabelValue,'Color','b','LabelVerticalAlignment','bottom');
+    PosRectinTF = [Param.TEMPCompinTF(1) Yl(1) Param.TEMPCompinTF(2)-Param.TEMPCompinTF(1) Yl(2)*0.1]; 
+    rectangle('Position',PosRectinTF,'FaceColor',[0 0 1 0.2],'EdgeColor',[0 0 1 0.2]);
+    LegendLabel = {'GFP';Param.CompN};
+    Lgd = legend(LegendLabel); title(Lgd,'Component','Color','k');
+
+    % 3. TOPOPLOTS
     subplot(2,3,[3 6]); 
     title(sprintf('Topography at %d TF',Value));            
     topoplotIndie(EEGTEMP(Value,:),Chanlocs);
